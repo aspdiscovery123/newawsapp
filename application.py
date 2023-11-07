28db60b67a94f76091adc7efa567fc2ca854fc92
aspdiscovery123 <48548207+aspdiscovery123@users.noreply.github.com>
2023-11-07 11:36:11 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello AWS CI/CD application"  # Use the return statement to specify the response
+    return "hey, it's working"
+
+@app.route('/home')
+def home1():
+    return "CI/CD pipeline"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
